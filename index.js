79ef6c502202cda6d167a3541707e476969a2ca8
trevor dewindt <trevor.dewindt@gmail.com>
2022-05-29 18:25:24 -0500

    commit tree 1a8971a77849ad4f6f807a107d8148d995058d3f

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+
+function writeCards(names, eventName) {
+    const thankMessages = [];
+
+    for (let i = 0; i < names.length; i++) {
+        thankMessages[i] = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+    }
+
+    return thankMessages;
+}
+
+
+function countDown(countDownInt) {
+    while (countDownInt >= 0) {
+        console.log(countDownInt);
+        countDownInt--;
+    }
+}
